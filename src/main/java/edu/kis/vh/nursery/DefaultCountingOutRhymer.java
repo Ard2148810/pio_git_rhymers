b1aa6c84a1b44ca9c05cc6ba13fca5a2bca3dc74
Adrian Alaszewski <wns2148810@interia.pl>
2020-01-31 19:34:49 +0100

    1.5 Tests, diagrams and docs

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,27 +2,27 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	final int NUM = -1;
-	final int NUM2 = 12;
+	private final int NUM = -1;
+	private final int NUM2 = 12;
 
 	private int[] numbers = new int[NUM2];
 
-	public int total = NUM;
+	private int total = NUM;
 
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
-	public boolean callCheck() {
+	boolean callCheck() {
 		return total == NUM;
 	}
 		
-	public boolean isFull() {
+	boolean isFull() {
 		return total == 11;
 	}
 		
-	protected int peekaboo() {
+	int peekaboo() {
 		if (callCheck())
 			return NUM;
 		return numbers[total];
--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * First In First Out
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-	
+	private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+	/**
+	 * Counts out in FIFO order
+	 * @return
+	 */
 	@Override
 	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
+		while (!callCheck()) {
+			temp.countIn(super.countOut());
+		}
 		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
+		while (!temp.callCheck()) {
+			countIn(temp.countOut());
+		}
 		return ret;
 	}
 }
